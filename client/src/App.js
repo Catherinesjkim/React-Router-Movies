20968b48d6caa8071bf11094920e18930d7a7aff
Catherine Kim <catherinesjkim@gmail.com>
2019-12-07 10:08:17 -0800

    Add Updated Index & App Files Because: - Was able to sho 6 movie cards on my browser window
    This commit:
    - Wrapped my app with the router
    - Inside my App file added two routes. One route for / that loads the Movie List component and one route that will take an id parameter after /movies/. This route loaded the Movie component.
    - Finished Part 1.
    - Will continue with Part 2 soon.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-
+import ReactDom from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import MovieList from './Movies/MovieList';
 import SavedList from './Movies/SavedList';
+import movie from "./Movies/Movie";
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -9,10 +12,16 @@ const App = () => {
     setSavedList( [...savedList, movie] );
   };
 
+  const [Movie] = useState();
+
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route path="/" component={MovieList} />
+      <Route
+        path="/movies/:movieId"
+        render={props => <Movie {...props} movies={movie} />}
+      /> 
     </div>
   );
 };
