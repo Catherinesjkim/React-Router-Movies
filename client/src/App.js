6e2babbe7b1e0e765e22b95d128ac572829232e3
Catherine Kim <catherinesjkim@gmail.com>
2019-12-08 15:14:08 -0800

    Add Updated MovieList js File Because: - Met MVP and fishined Part 1 & 2
    This commit:
    - Made it so that the card in MovieList is a link, this directs the user to the /movies/{id of movie here} URL, where :id is the id of the individual movie.
    - When a user clikcs on a movie card they are taken to /movies/{id of movie here} to see the details for the selected movie.
    - Added functionality so the Home button on the SavedList component navigates back to home.
    - I'm now able to navigate back and forth between the individual movies and the home screen.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import ReactDom from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import MovieList from './Movies/MovieList';
+import { Route, Router, Link } from "react-router-dom";
 import SavedList from './Movies/SavedList';
-import movie from "./Movies/Movie";
+import MovieList from './Movies/MovieList';
+import Movies from "./Movies/Movie";
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -15,13 +15,10 @@ const App = () => {
   const [Movie] = useState();
 
   return (
-    <div>
+    <div className="nav-links">
       <SavedList list={savedList} />
-      <Route path="/" component={MovieList} />
-      <Route
-        path="/movies/:movieId"
-        render={props => <Movie {...props} movies={movie} />}
-      /> 
+      <Route exact path="/" component={MovieList} /> 
+      <Route path="/movies/:id" component={Movies} />
     </div>
   );
 };
